4257a5ec2db9b6a951154c24b543ae7bb5f826e0
Jonathan Alexander <jonathandalexander@gmail.com>
2017-09-09 20:43:50 -0400

    vid

--- src/index.js
+++ src/index.js
@@ -3,4 +3,22 @@ import scss from './styles/app.scss';
 
 let $ = jQuery
 
-$('body').append('<h1>yo</h1>')
+const video = $('<video />', {
+    id: 'video',
+    src: 'https://media.giphy.com/media/OcO1mg91Ct4CA/giphy.mp4',
+    type: 'video/mp4',
+    controls: false,
+    autoplay: true,
+    loop: true
+});
+
+$('body').append(video)
+
+/*
+
+<video poster="https://media.giphy.com/media/OcO1mg91Ct4CA/200_s.gif" style="margin:0;padding:0" width="500" height="373" autoplay="" loop="">
+  <source src="https://media.giphy.com/media/OcO1mg91Ct4CA/giphy.mp4" type="video/mp4">
+  Your browser does not support the mp4 video codec.
+</video>
+
+*/
